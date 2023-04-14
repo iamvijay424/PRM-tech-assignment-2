364a97a0b51c98e7a73ab4e4c25d261f3796021b
iamvijay424 <117527225+iamvijay424@users.noreply.github.com>
2023-04-14 09:55:15 +0530

    Add files via upload

--- PRM tech assignment 2.py
+++ PRM tech assignment 2.py
@@ -1,28 +1,3 @@
-
-# Your task is to complete the validate_triangle and the validate_rectangle functions for the classes. 
-# Hint for validating is given in the comments of the code. Also you will have to fill the following after validation in respective functions:
-
-# Invalid triangle: If the triangle sum property of sides is not valid (more hints in the comments of the code)
-# Valid Triangle: If the triangle sum property of sides is valid
-# Valid rectangle: If 2 side pairs are same and they are input in correct order like l,b,l,b
-# Invalid rectangle: If Not Valid rectangle as stated above
-
-# Input format
-# The side length of the triangle followed by for rectangle in the next line in order
-
-# Output format:
-# Since objects are created in order, so first validate info about triangle will come and then rectangle
-
-# Sample input:0
-# 3 4 5
-# 2 4 2 4
-
-# Sample Output:0
-# Valid Triangle
-# Valid Rectangle
-
-
-
 class Triangle():
     def __init__(self,lst):
         self.lst=lst
@@ -31,7 +6,6 @@ class Triangle():
             return "Valid Triangle"         
         else:
             return "Invalid Triangle"
-s=list(map(int,input().split()))
 class Rectangle():
     def __init__(self,lst):
         self.lst=lst
@@ -40,8 +14,8 @@ class Rectangle():
             return "Valid Rectangle"         
         else:
             return "Invalid Rectangle"
-# s=list(map(int,input().split()))
-t=list(map(int,input().split()))
+s=list(map(int,input('Enter the triangle sides separate with space :').split()))
+t=list(map(int,input('Enter the rectangle sides separte with space :').split()))
 A=Triangle(s)       
 print(A.validate_triangle(s))
 B=Rectangle(t)
